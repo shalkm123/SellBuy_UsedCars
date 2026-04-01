c450e8502dd416e51a8b653eaa9fab3ef4db8a5b
shalkm123 <km.shal0123@gmail.com>
2026-04-01 16:18:02 +0530

    initial commit

---
+++ used-cars-marketplace/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Used Cars Selling and Buying</h1>
+        <p>Find your perfect used car or list your vehicle for sale</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
